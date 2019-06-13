4425bef325f531b6c5ebba460aa5063d473dbb1c
Paweł Nieczuja-Ostrowski <pawel.nieczuja@onet.eu>
2019-06-13 14:37:49 +0200

    Add js navbar

---
+++ js/script.js
@@ -0,0 +1,24 @@
+"use strict"
+
+//button-hamburger
+const navbarBtn = document.getElementsByClassName('hamburger');
+navbarBtn[0].addEventListener("click", functionNavbar); 
+
+function functionNavbar() {     
+  const navbarLinks = document.getElementsByClassName("nav_links");
+  navbarLinks[0].classList.toggle("nav_links_added");
+
+  const elem1 = document.getElementsByClassName("hamburger_elem1");
+  elem1[0].classList.toggle("hamburger_elem1_added");
+  
+  const elem2 = document.getElementsByClassName("hamburger_elem2");
+  elem2[0].classList.toggle("hamburger_elem2_added");
+  
+  const elem3 = document.getElementsByClassName("hamburger_elem3");
+  elem3[0].classList.toggle("hamburger_elem3_added");
+}
+
+const linkClick = document.getElementsByClassName('nav_links');
+linkClick[0].addEventListener("click", functionNavbar); 
+
+
